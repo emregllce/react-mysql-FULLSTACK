00122303032edf18d7c4a14461a571c87e5bbffd
emregllce <emregllce@gmail.com>
2022-07-17 00:41:25 +0300

    erased unnecessary

--- src/approuter/AppRouter.js
+++ src/approuter/AppRouter.js
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./AppRouter.css"
 import Footer from "../component/Footer";
-import NotFound from "../pages/NotFound";
 import ProductAdd from "../pages/ProductAdd";
 import ProductList from "../pages/ProductList";
-import ProductStyle from "../pages/ProductStyle";
 
 const AppRouter = () => {
   return (
@@ -15,8 +13,6 @@ const AppRouter = () => {
           <Routes>
             <Route path="/" element={<ProductList />} />
             <Route path="/addproduct" element={<ProductAdd />} />
-            <Route path="/style" element={<ProductStyle />} />
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
         <Footer />
--- src/pages/ProductStyle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProductStyle = () => {
-  return (
-    <div>ProductStyle</div>
-  )
-}
-
-export default ProductStyle
