423e5afb0051dd2568415324449edc26d0085886
emregllce <emregllce@gmail.com>
2022-07-24 12:45:05 +0300

    test

--- src/pages/ProductAdd.js
+++ src/pages/ProductAdd.js
@@ -103,8 +103,8 @@ const handleBack = () => {
               <option id="DVD" value="DVD">
                 DVD
               </option>
-              <option id="Book" value="NameTest000">
-              Book
+              <option id="Book" value="Book">
+                Book
               </option>
               <option id="Furniture" value="Furniture">
                 Furniture
